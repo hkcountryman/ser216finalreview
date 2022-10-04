e7f0ee992cd49e4ae10f389399dc69ebfa322a35
Halle Countryman <hkcountryman@gmail.com>
2022-10-04 12:44:40 -0700

    Tests

---
+++ Example.java
@@ -0,0 +1,34 @@
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Example {
+
+    /**
+     * Multiplies all elements of a list of integers by some other integer and returns the result.
+     * 
+     * @param numbers the list of numbers
+     * @param multiplier the scalar to multiply by
+     * @return a new list
+     */
+    public static List<Integer> multiplyAll(List<Integer> numbers, int multiplier) {
+        if (numbers.isEmpty()) {
+            return numbers;
+        }
+        return numbers.stream().map(num -> num * multiplier).collect(Collectors.toList());
+    }
+
+    /**
+     * Divides all elements of a list of integers by some other integer and returns the result.
+     * 
+     * @param numbers the list of numbers
+     * @param divider the scalar to divide by
+     * @return a new list
+     */
+    public static List<Integer> divideAll(List<Integer> numbers, int divider) {
+        if (numbers.isEmpty()) {
+            return numbers;
+        }
+        return numbers.stream().map(num -> num / divider).collect(Collectors.toList());
+    }
+
+}
---
+++ ExampleTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class ExampleTest {
+
+    // Set up some lists... what would be good for equivalence partitioning?
+
+    @BeforeClass
+    public static void init() {
+        // Initialize those lists
+    }
+
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+
+    // Make example tests...
+
+}
