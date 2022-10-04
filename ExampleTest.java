dda67b0b4bd19643b4d99e265e15b03a051f4823
Halle Countryman <hkcountryman@gmail.com>
2022-10-04 15:23:33 -0700

    Take away hints

--- ExampleTest.java
+++ ExampleTest.java
@@ -1,3 +1,4 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -6,14 +7,8 @@ public class ExampleTest {
 
     // Set up some lists... what would be good for equivalence partitioning?
 
-    @BeforeClass
     public static void init() {
         // Initialize those lists
-    }
-
-    @Test
-    public void test() {
-        assertTrue(true);
     }
 
     // Make example tests...
